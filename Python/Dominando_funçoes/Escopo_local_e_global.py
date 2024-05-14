798d984c159f2b3febeb916bb37a13e0b2861547
ohhigordev <higor.nascimento0704@gmail.com>
2024-05-13 21:36:41 -0300

    bdsfn

--- Python/Dominando_funçoes/Escopo_local_e_global.py
+++ Python/Dominando_funçoes/Escopo_local_e_global.py
@@ -2,9 +2,10 @@ salario = 2000
 
 
 def salario_bonus(bonus):
-    global salario
+    global salario # isso serve para a funcção buscar valores que estão fora dela, ou seja, valores globais.
     salario += bonus
     return salario
 
 
-salario_bonus(500)  # 2500
+salario_bonus(500)  # 25000
+
