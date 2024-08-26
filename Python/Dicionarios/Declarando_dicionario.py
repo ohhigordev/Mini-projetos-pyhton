4670165c51fa8aadd83b32778f040c098db063b3
ohhigordev <higor.nascimento0704@gmail.com>
2024-08-26 10:46:09 -0300

    python

--- Python/Dicionarios/Declarando_dicionario.py
+++
@@ -1,8 +0,0 @@
-pessoa = {"nome": "Guilherme", "idade": 28}
-print(pessoa)
-
-pessoa = dict(nome="Guilherme", idade=28)
-print(pessoa)
-
-pessoa["telefone"] = "3333-1234"  # {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
-print(pessoa)
--- Python/Estruturas de dados/list.append.py
+++
@@ -1,9 +0,0 @@
-
-
-
-# listen.append(x) - só adiciona ao final da lista.
-
-cidades = ['São Paulo', 'Rio de Janeiro', 'Salvador', 'Goiania', 'Brasilia']
-
-cidades.append('Pindoretama')
-print(cidades)
